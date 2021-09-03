0fa5a08ec69bd2936a3b69b38e99a4b410f8ccc3
ldifalco <luke.difalco52@gmail.com>
2021-09-03 17:37:17 -0400

    completed models file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,30 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { canTreatArrayAsAnd } = require('sequelize/types/lib/utils');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  }
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+  foreignKey: 'tag_id',
+  }
+});
 
 module.exports = {
   Product,
